15f71799b3b773c7696e2bcdbc19c4b2696d431b
Eti Reznikov <44700034+EtiReznikov@users.noreply.github.com>
2020-04-06 13:05:45 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,12 +1,12 @@
-@inProceedings{kanazawaHMR18,
-  title={End-to-end Recovery of Human Shape and Pose},
-  author = {Angjoo Kanazawa
-  and Michael J. Black
-  and David W. Jacobs
-  and Jitendra Malik},
-  booktitle={Computer Vision and Pattern Recognition (CVPR)},
-  year={2018}
-}
+# @inProceedings{kanazawaHMR18,
+#   title={End-to-end Recovery of Human Shape and Pose},
+#   author = {Angjoo Kanazawa
+#   and Michael J. Black
+#   and David W. Jacobs
+#   and Jitendra Malik},
+#   booktitle={Computer Vision and Pattern Recognition (CVPR)},
+#   year={2018}
+# }
 
 from __future__ import absolute_import
 from __future__ import division
